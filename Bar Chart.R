5ccd53b44dcc3a03813e96865fd6ddc694f6b48b
Tavo <gariosp@unal.edu.co>
2018-05-26 20:58:46 -0300

    Bar chart in R

---
+++ Bar Chart.R
@@ -0,0 +1,17 @@
+rm(list=ls())
+
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
+library("readxl")
+
+#Importa Archivo para trabajar
+IMCInfantil = read_excel("../Data Sets Apunte Teórico/IMCinfantil.xlsx")
+
+#Se pone la base en memoria
+attach(IMCInfantil)
+
+#Gráfica diagrama de barras
+barplot(table(CatPeso), 
+        ylab = ("Cantidad"),
+        names.arg = c("Deficiente", "Normal", "Obeso", "Con_Sobrepeso"),
+        col = c("palegreen1", "paleturquoise", "plum2", "lightpink1"))
