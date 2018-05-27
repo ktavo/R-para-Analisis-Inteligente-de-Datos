877ae4dc53082ea180a4eee46d13172cf46450e0
Tavo <gariosp@unal.edu.co>
2018-05-26 21:27:51 -0300

    Overlapping bars scipt

---
+++ Overlapping_Bars.R
@@ -0,0 +1,20 @@
+#
+
+rm(list=ls())
+
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
+
+library("readxl")
+library("ggplot2")
+
+#Importa Archivo para trabajar
+IMCInfantil = read_excel("../Data Sets Apunte Teórico/IMCinfantil.xlsx")
+
+datos = data.frame(table(SEXO, CatPeso))
+
+ggplot(data = datos, aes(x=CatPeso, y=Freq, fill=SEXO)) +
+  geom_bar(stat="identity", colour="blue") +
+  scale_fill_brewer(palette="Paired") +
+  xlab("Categoria_de_peso") +
+  ylab("")
