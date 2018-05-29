45a178ae1117594d4b31e6f2805e5e1492688119
Tavo <gariosp@unal.edu.co>
2018-05-29 17:56:54 -0300

    Addig box-plot script modifications

--- Mosaic_Graph.R
+++ Mosaic_Graph.R
@@ -1,6 +1,23 @@
 #Mosaic graph
 #Se utiliza para representar distribuciones conjuntas multivariadas
 
-
 rm(list=ls())
 setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
+#Carga de datos
+gar.no = c(258, 280)
+gar.si = c(184, 719)
+
+#Combina los datos
+mat = rbind(gar.no, gar.si)
+
+#Pone nombre a las columnas
+colnames(mat) = c("No_Considera_Consumo", "Considera_Consumo")
+#Pone nombre a las filas
+rownames(mat) = c("No_Considera_Garantía", "Considera_Garantía")
+
+#Genera Diagrama de Mosaicos
+mosaicplot(mat, col = c("skyblue", "royalblue"), cex.axis = 0.8, main = "")
+
+
+
