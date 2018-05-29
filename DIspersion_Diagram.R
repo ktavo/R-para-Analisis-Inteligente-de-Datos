cbf34fd2167fe60b7123d1664cdcf53d820ca1db
Tavo <gariosp@unal.edu.co>
2018-05-29 18:08:47 -0300

    Adding dispersion diagram base

---
+++ DIspersion_Diagram.R
@@ -0,0 +1,7 @@
+#Dispersion Diagram
+#Permite apreciar las carácteristidas de las variables de los dos ejes
+#Permite ver agrupaciones de individuos de acuerdo a las dos variables de los ejes
+
+rm(list=ls())
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
