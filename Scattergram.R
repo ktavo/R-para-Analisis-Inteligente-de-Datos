c138be26aca334bf0fdf98ce613455bc255a3273
Tavo <gariosp@unal.edu.co>
2018-05-29 22:25:06 -0300

    Adding scatterplot script

--- Scattergram.R
+++ Scattergram.R
@@ -4,3 +4,15 @@
 
 rm(list=ls())
 setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+detach(IMCInfantil)
+
+library("readxl")
+
+#Importa Archivo para trabajar
+IMCInfantil = read_excel("../Data Sets Apunte Teórico/IMCinfantil.xlsx")
+attach(IMCInfantil)
+
+#Arma una base con as variables numéricas
+base.niños = data.frame(EDAD, PESO, TALLA, IMC, CC)
+#rRoduce un diagrama de dispersión de a pares
+pairs(base.niños, pch = 19, cex = 0.8, col = cm.colors(dim(base.niños)[2]))
