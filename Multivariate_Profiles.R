e40f60ecd5279fb40758f84478078a0eb57d14a3
Tavo <gariosp@unal.edu.co>
2018-05-30 21:56:27 -0300

    Adding Multivariate profiles base

---
+++ Multivariate_Profiles.R
@@ -0,0 +1,6 @@
+#Multivariate profiles
+#Permite comparar la posición central de varias variables en distintos individuos
+
+rm(list=ls())
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
