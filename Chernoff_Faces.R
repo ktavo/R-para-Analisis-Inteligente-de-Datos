13521399f1e6c014aacb70c9cd0de6d989128639
Tavo <gariosp@unal.edu.co>
2018-05-31 17:49:23 -0300

    Base for Chernoff faces Script

---
+++ Chernoff_Faces.R
@@ -0,0 +1,6 @@
+#Chernoff Faces
+#Los diversos datos se transforman en caras, sirve para identificar similitudes entre individuos
+
+rm(list=ls())
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
