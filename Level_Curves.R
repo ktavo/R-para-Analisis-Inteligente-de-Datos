f5f7236e563c2f2396dbbbce45c5ce7b7a61b9b4
Tavo <gariosp@unal.edu.co>
2018-05-31 15:08:51 -0300

    Adding Level curves script base

---
+++ Level_Curves.R
@@ -0,0 +1,6 @@
+#Level Curves
+#Los colores y las lineas de contorno ayudan a identificar regiones de mayor densidad de observaciones.
+#Mostramos el caso de la distribución Normal Bivariada en las Figuras
+
+rm(list=ls())
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
