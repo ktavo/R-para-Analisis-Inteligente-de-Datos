3d3db4c9da2ee929a8e14a53bfbde31a832c55d3
Tavo <gariosp@unal.edu.co>
2018-05-31 20:39:11 -0300

    Base for univariatecontrol script

---
+++ Univariate_Control.R
@@ -0,0 +1,11 @@
+#univariate control
+#Si observamos el comportamiento de una de las variables por separado, podemos detectar si 
+#alguna de ellas se ha ido de rango.
+
+#Paquete para dibujos
+library("ggplot2")
+#Paquete para manipular datos
+library("dplyr")
+#Paquete para leer archivos xlsx
+library("readxl")
+
