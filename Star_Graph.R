a91c20af7d98e44497f39424218c94a65f5d3e6a
Tavo <gariosp@unal.edu.co>
2018-05-31 16:48:29 -0300

    Star Graph script

--- Star_Graph.R
+++ Star_Graph.R
@@ -2,3 +2,15 @@
 #Cuando todas las variables consideradas son cuantitativas para poder detectar estructuras similares,
 #es adecuado el gráfico de estrellas.
 
+rm(list=ls())
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
+#Toma las primeras nueve marcas de la base
+autos = mtcars[1:9,]
+
+#Añadimos etiquetas
+row.names(autos) = c("Mazda", "Mazda_Wag", "Datsun", "Hornet_D", "Hornet_S",
+                     "Valiant", "Duster", "Merc_D", "Merc")
+
+#Generamos diagrama de estrellas
+stars(autos, full = F, cex = 0.8, flip.labels = T, len = 1.9, col.stars = cm.colors(9))
