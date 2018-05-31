9cd599b3f5f508f25ece4fbe3ce7f5f2479a8a4c
Tavo <gariosp@unal.edu.co>
2018-05-31 18:47:16 -0300

    Chernof faces script

--- Chernoff_Faces.R
+++ Chernoff_Faces.R
@@ -4,3 +4,16 @@
 rm(list=ls())
 setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
 
+library("tcltk2")
+library("aplpack")
+library("readxl")
+
+#Importa Archivo para trabajar
+galletitas = read_excel("../Data Sets Apunte Teórico/galletitas.xlsx")
+attach(galletitas)
+
+#Agrupamos las galletitas saladas
+saladas = split(galletitas, galletitas$Tipo)$salada
+
+#Genera las caras de Chernoff
+faces(saladas[,2:6], nrow.plot = 2, ncol.plot = 5, face.type = 1, labels = saladas$Marca)
