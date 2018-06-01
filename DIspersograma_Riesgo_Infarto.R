34e21667ddc45ae2f2c25a857f46646609c9d917
Tavo <gariosp@unal.edu.co>
2018-06-01 13:36:52 -0300

    Script to create two variables dispersogram for heart disease risk

--- DIspersograma_Riesgo_Infarto.R
+++ DIspersograma_Riesgo_Infarto.R
@@ -3,7 +3,25 @@
 rm(list=ls())
 setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
 
-#Importa Archivo para trabajar
-pacientes = read_excel("../Data Sets Apunte Teórico/riesgo.xlsx")
-attach(pacientes)
+install.packages("ggrepel")
+#Libraría para graficar
+library("ggplot2")
+#Librería para manipular etiquetas de los gráficos
+library("ggrepel")
+#Librería para leer xlsx
+library("readxl")
 
+#Importa Archivo para trabajar
+riesgo = read_excel("../Data Sets Apunte Teórico/riesgo.xlsx")
+attach(riesgo)
+
+#Generamos el dispersograma
+ggplot(riesgo, aes(x = Peso, y = Superficie)) +
+  geom_point(colour = "royalblue", shape = 8) +
+  xlab("Peso") +
+  ylab("Superficie_corporal") +
+  geom_text_repel(aes(label = rownames(riesgo), size = 2)) +
+  theme(legend.position = "none")
+  
+  
+  
