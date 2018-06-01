2954bbea3afb49bb0662f977036c4d5dc41109d5
Tavo <gariosp@unal.edu.co>
2018-05-31 21:34:16 -0300

    Clarifying scatterplot script

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -11,19 +11,19 @@ library(MASS)
 
 #Genera los datos
 dat = mvrnorm(n = 60,c(10,5), cbind(c(0.7,0.5), c(0.5,0.4)),tol = 1e-6,
-        empirical=FALSE,EISPACK=FALSE)
+        empirical = FALSE, EISPACK = FALSE)
 
 #Arregla los datos
 datos=data.frame(dat)
 
 #Produce un diagrama
-ggplot(datos,aes(x=X1,y=X2))+
+ggplot(datos,aes(x = X1,y = X2))+
   geom_point(colour="royalblue")+
-  geom_point(aes(x=11.6,y=3.3),colour="indianred3")+
-  stat_ellipse(aes(x=X1,y=X2),colour="orchid3",type="norm")+
-  geom_hline(yintercept=3,linetype="dashed",colour="forestgreen")+
-  geom_hline(yintercept=7,linetype="dashed",colour="forestgreen")+
-  geom_vline(xintercept=8,linetype="dashed",colour="forestgreen")+
-  geom_vline(xintercept=12,linetype="dashed",colour="forestgreen")+
+  geom_point(aes(x = 11.6,y = 3.3),colour = "indianred3")+
+  stat_ellipse(aes(x = X1,y = X2),colour ="orchid3",type="norm")+
+  geom_hline(yintercept = 3,linetype = "dashed",colour = "forestgreen")+
+  geom_hline(yintercept = 7,linetype = "dashed",colour = "forestgreen")+
+  geom_vline(xintercept = 8,linetype = "dashed",colour = "forestgreen")+
+  geom_vline(xintercept = 12,linetype = "dashed",colour = "forestgreen")+
   xlab("")+
   ylab("")
