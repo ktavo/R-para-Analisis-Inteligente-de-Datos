9d5888250b60d0023e29848b526da469389eb97e
Tavo <gariosp@unal.edu.co>
2018-06-01 13:17:25 -0300

    Adding dispersograma para riesgo cardiaco

---
+++ DIspersograma_Riesgo_Infarto.R
@@ -0,0 +1,9 @@
+#Dispersograma dos variables
+
+rm(list=ls())
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
+#Importa Archivo para trabajar
+pacientes = read_excel("../Data Sets Apunte Teórico/riesgo.xlsx")
+attach(pacientes)
+
