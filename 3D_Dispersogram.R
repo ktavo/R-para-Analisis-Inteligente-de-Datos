44ceba559164dc0c7204ed84ca3d23312d23250f
Tavo <gariosp@unal.edu.co>
2018-07-21 18:33:06 -0300

    Adding 3dDispersogram by sex, csv needed to be modified to add Sexo column

--- 3D_Dispersogram.R
+++ 3D_Dispersogram.R
@@ -10,6 +10,7 @@ library("readxl")
 #Permite realizar gráficos simultáneos
 par(mfrow = c(1,2))
 
+
 riesgo = read_excel("../Data Sets Apunte Teórico/riesgo.xlsx")
 
 scatterplot3d(riesgo[,2:4], angle = 35, pch = 16, color = "royalblue", box = FALSE, grid = TRUE,
---
+++ 3d_Dispersogram_By_Sex.R
@@ -0,0 +1,19 @@
+#Dispersograma tres variables
+
+rm(list=ls())
+setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
+
+#Librería para graficar
+library("scatterplot3d")
+library("readxl")
+
+riesgo = read_excel("../Data Sets Apunte Teórico/riesgo.xlsx")
+
+#Organiza los datos
+datos = data.frame(x = riesgo$Presión, y = riesgo$Peso, z = riesgo$Edad, group = riesgo$Sexo)
+
+#Dispersograma separado por grupos
+with(datos, scatterplot3d(x, y, z, box = FALSE, grid = TRUE, pch = 16, 
+                          color = ifelse(group == "M", "royalblue", "indianred3"),
+                          xlab = "Presión", ylab = "Peso", zlab = "Edad"))
+legend("topright", legend = unique(riesgo$Sexo), title = "Sexo", pch = 16, col = c("indianred3", "royalblue"))
