ce8ff712b9c0302303f1432c1499b0c4424cf59e
Tavo <gariosp@unal.edu.co>
2018-07-26 20:59:43 -0300

    Generacion data frame colesterol conejos

--- Comparacion_Medias.R
+++ Comparacion_Medias.R
@@ -3,9 +3,19 @@
 rm(list=ls())
 setwd("E:/UBA/Análisis Inteligente de Datos/Scripts Apunte Teórico")
 
+
 #Colesterol Conejo
 dieta1 = c(13.4, 11, 15.3, 16.7, 13.4, 20.1, 13.6, 18.3)
 dieta2 = c(10.4, 14.2, 20.5, 19.6, 18.5, 24, 23.4, 13.6)
 dieta3 = c(7.5, 7.2, 6.7, 7.6, 11.2, 9.6, 6.8, 8.5)
+dietas = c(dieta1, dieta2, dieta3)
 ColesterolConejos = rbind(dieta1, dieta2, dieta3)
-t(ColesterolConejos)
+ColesterolConejos <- t(ColesterolConejos)
+summary(ColesterolConejos)
+
+boxplot(ColesterolConejos, col = topo.colors(3), main = "Diámetro aórtico por dieta")
+
+ColesterolConejos <- as.data.frame(ColesterolConejos, 
+                      row.names = dietas,stringsAsFactors = default.stringsAsFactors())
+
+#colesterol.aov = aov(ColesterolConejos ~ c(dieta1, dieta2, dieta3), data = ColesterolConejos)
